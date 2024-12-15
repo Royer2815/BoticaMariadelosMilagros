f281966b40d2581b245024e166eb19d47d0cc48a
IsaacEmerson-05 <72278816@ijamvirtual.edu.pe>
2024-12-14 23:41:42 -0500

    Avance3

---
+++ CapaDatos/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Producto
+    {
+        public int Id { get; set; } // Identificador único del producto  
+        public string Nombre { get; set; } // Nombre del producto  
+        public decimal Precio { get; set; } // Precio del producto  
+        public int Stock { get; set; } // Cantidad disponible en stock  
+    }
+}
