e2ef2d586b68754148c59b6f33382dee3be19b24
Marco Antonio <77245420@ijamvirtual.edu.pe>
2024-12-29 01:30:42 -0500

    conexion con la base de datos

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Conexion
+{
+    static void Main()
+    {
+        
+        string connectionString = @"Server=localhost\DESKTOP-E720FUG\SQLEXPRESS;Database=DB_botica_milagros;Integrated Security=True";
+
+        
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                // Abrir la conexión
+                connection.Open();
+
+                
+                SqlCommand command = new SqlCommand("SELECT * from Empleado", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0].ToString()); 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
